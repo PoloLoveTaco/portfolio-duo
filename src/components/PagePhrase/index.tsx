53328c8895cffcfea7fb175c5a0222a584f0397d
Polo <jules.trolle@epitech.eu>
2026-06-15 09:30:47 +0200

    [ADD] type for props for component

--- src/components/PagePhrase/index.tsx
+++ src/components/PagePhrase/index.tsx
@@ -1,7 +1,13 @@
-export default function PagePhrase(props) {
-    return (
-        <div>
-            <h1>This is the <span style={{ color: "red" }}>{props.title}</span> page</h1>
-        </div>
-    );
+type PagePhraseProps = {
+    title: string;
+};
+
+export default function PagePhrase(props: PagePhraseProps) {
+  return (
+    <div>
+        <h1>
+            This is the <span style={{ color: "red" }}>{props.title}</span> page
+        </h1>
+    </div>
+  );
 }
